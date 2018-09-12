77adf5491abd366004e40adb1097d29338e83e2f
windynik <nikhilprem@rocketmail.com>
2018-09-12 22:45:29 +0530

    Added reducers for redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,12 @@ const API_KEY='84158d0f6c41aff592b1fc87b592a479'
 const api=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER='FETCH_WEATHER';
+
 export function fetchWeather(city){
 
     const url=`${api}&q=${city},us`;
     const request=axios.get(url);
-
+    console.log('Request : ',request)
     return ({
         type: FETCH_WEATHER,
         payload: request
